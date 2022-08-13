1fb6f4b0563745a31320b2dae868f9dc18528a63
Nick Newton <nickanewton@gmail.com>
2022-08-13 13:45:38 -0700

    added heading with app name and font family.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import { ChakraProvider, Box } from '@chakra-ui/react';
+import { ChakraProvider, Box, Heading} from '@chakra-ui/react';
 import { appTheme } from './styles/Theme'
 
 function App() {
   return (
     <ChakraProvider theme={appTheme}>
       <Box bgColor={appTheme.colors.body} minHeight="100vh">
+        <Heading textAlign='center' fontFamily='La Belle Aurore, cursive'>Today's Tasks</Heading>
       </Box>
     </ChakraProvider>
   );
