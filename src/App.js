d0a728c7453fbd232fee37454bc296bf313fe858
Nick Newton <nickanewton@gmail.com>
2022-08-15 14:03:57 -0700

    Completed initial stylying of add task.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
-import { ChakraProvider, Box, Heading} from '@chakra-ui/react';
+import { ChakraProvider, Box, Heading, Flex, Input, Button, Container } from '@chakra-ui/react';
 import { appTheme } from './styles/Theme'
 
 function App() {
   return (
     <ChakraProvider theme={appTheme}>
-      <Box bgColor={appTheme.colors.body} minHeight="100vh">
-        <Heading textAlign='center' fontFamily='La Belle Aurore, cursive'>Today's Tasks</Heading>
+      <Container>
+      <Box minHeight="100vh">
+        <Heading 
+          textAlign='center' 
+          fontFamily={appTheme.fonts.family.laBelle} 
+          fontSize={appTheme.fonts.size.heading}
+          mt="2.5rem">  
+          Today's Tasks
+        </Heading>
+        <Flex mt="2rem">
+          <Input bgColor="#FFFFFF" mr="0.75rem" />
+          <Button bgColor="#B1D19B" color="#FFFFFF">ADD</Button>
+        </Flex>
       </Box>
+      </Container>
     </ChakraProvider>
   );
 }
